93120487d2a9b9f51ade2d5920ea8f76a31f6c7f
fensziii <martincry@gmail.com>
2021-10-12 19:52:48 +0200

    Update x16rt.c

--- algos/x16rt.c
+++ algos/x16rt.c
@@ -3,22 +3,22 @@
 #include <string.h>
 
 #include "sha256.h"
-#include <sha3/sph_blake.h>
-#include <sha3/sph_bmw.h>
-#include <sha3/sph_groestl.h>
-#include <sha3/sph_jh.h>
-#include <sha3/sph_keccak.h>
-#include <sha3/sph_skein.h>
-#include <sha3/sph_luffa.h>
-#include <sha3/sph_cubehash.h>
-#include <sha3/sph_shavite.h>
-#include <sha3/sph_simd.h>
-#include <sha3/sph_echo.h>
-#include <sha3/sph_hamsi.h>
-#include <sha3/sph_fugue.h>
-#include <sha3/sph_shabal.h>
-#include <sha3/sph_whirlpool.h>
-#include <sha3/sph_sha2.h>
+#include "sha3/sph_blake.h"
+#include "sha3/sph_bmw.h"
+#include "sha3/sph_groestl.h"
+#include "sha3/sph_jh.h"
+#include "sha3/sph_keccak.h"
+#include "sha3/sph_skein.h"
+#include "sha3/sph_luffa.h"
+#include "sha3/sph_cubehash.h"
+#include "sha3/sph_shavite.h"
+#include "sha3/sph_simd.h"
+#include "sha3/sph_echo.h"
+#include "sha3/sph_hamsi.h"
+#include "sha3/sph_fugue.h"
+#include "sha3/sph_shabal.h"
+#include "sha3/sph_whirlpool.h"
+#include "sha3/sph_sha2.h"
 
 #include "Common.h"
 
